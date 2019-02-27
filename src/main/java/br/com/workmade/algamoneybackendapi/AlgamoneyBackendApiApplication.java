cc7a6bb31ac13f5fefcb7cf0d2e2bd5fc3748c9d
Marcos Ferreira <marcosradix@gmail.com>
2019-02-26 21:50:34 -0300

    primeiro commit

---
+++ src/main/java/br/com/workmade/algamoneybackendapi/AlgamoneyBackendApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.workmade.algamoneybackendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoneyBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoneyBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/workmade/algamoneybackendapi/AlgamoneyBackendApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.workmade.algamoneybackendapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgamoneyBackendApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
