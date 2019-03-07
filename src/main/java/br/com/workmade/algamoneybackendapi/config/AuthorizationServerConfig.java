5536c9409d28a12cf8cde29f055c83798f8d56da
Marcos Ferreira <marcosradix@gmail.com>
2019-03-07 00:31:10 -0300

    usando token jwt para autenticar

--- src/main/java/br/com/workmade/algamoneybackendapi/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/workmade/algamoneybackendapi/config/AuthorizationServerConfig.java
@@ -9,8 +9,11 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -36,6 +39,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Value(value = "${expire.time.token}")
 	private Integer expireTimeToken;
 	
+	@Value(value = "${validation.token.secret}")
+	private String validationTokenSecret;
+	
 	@Override // authentication application
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient(this.nameClientApplication)
@@ -49,12 +55,20 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
 		.authenticationManager(this.authenticationManager);
+	}
+
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey(this.validationTokenSecret);
+		return accessTokenConverter;
 	}
 
 	@Bean //store the token
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(this.accessTokenConverter());
 	}
 	
 	
